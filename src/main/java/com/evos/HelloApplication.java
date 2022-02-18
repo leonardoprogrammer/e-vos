1266ae029c652229fa1fd8380113ea6c2227d115
Leonardo Silva <81488877+leonardoprogrammer@users.noreply.github.com>
2022-02-18 00:22:52 -0300

    Criação do layout da tela de login

--- src/main/java/com/evos/HelloApplication.java
+++ src/main/java/com/evos/HelloApplication.java
@@ -2,6 +2,7 @@ package com.evos;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,8 +11,14 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("formLogin.fxml"));
+        /*FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("formLogin.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("E-VOS - Login");
+        stage.setScene(scene);
+        stage.show();*/
+
+        Parent root = FXMLLoader.load(getClass().getResource("/main/resources/gui/formLogin.fxml"));
+        Scene scene = new Scene(root, 600, 400);
         stage.setTitle("E-VOS - Login");
         stage.setScene(scene);
         stage.show();
